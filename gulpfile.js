ddbf3af050434fd24839bfde7d0966958dcb0d2e
Yufenyuy Veyeh Didier <yveyeh@gmail.com>
2021-09-07 17:56:27 +0100

    v1.0.0-alpha.1

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,20 @@ const plumber = require('gulp-plumber')
 const ts = require('gulp-typescript')
 const tsProject = ts.createProject('tsconfig.json')
 
-const buildCSS = function () {
+const buildCSS = () => {
     return src('src/scss/ice-cookies.scss')
+        .pipe(sass({outputStyle: 'compact'}))
+        .pipe(dest('dist/ice-cookies/'))
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(rename({extname: '.min.css'}))
         .pipe(dest('dist/ice-cookies/'))
 }
 
-const buildJS = function () {
+const buildJS = () => {
     return src('src/ts/ice-cookies.ts')
         .pipe(plumber())
         .pipe(tsProject())
+        .pipe(dest('dist/ice-cookies/'))
         .pipe(uglify())
         .pipe(rename({extname: '.min.js'}))
         .pipe(dest('dist/ice-cookies/'))
